02d820555da49ebf85a99a04fd97b3039bf63a4e
Alek Harden <alekmharden@gmail.com>
2021-04-19 23:49:20 -0400

    windows/linux ifdef test

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,33 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <fstream>
+#include <map>
+
+
+
+
+
+
+void readSettings(std::map<std::string,struct setting>,std::string fileName);
+
+struct setting{
+    std::string preset;
+    std::string current;
+};
+
 
 int main(){
+
+    std::map<std::string,struct setting> settings;
+    settings.insert( std::pair<std::string,struct setting>("fullscreen",{"false","false"}));
+    settings.insert( std::pair<std::string,struct setting>("width",{"854","854"}));
+    settings.insert( std::pair<std::string,struct setting>("height",{"480","480"}));
+
+
+    std::cout << "----" << std::endl;
+    readSettings(settings,"settings.txt");
+
 
 
     if (!glfwInit()) throw "Error: GLFW could not initialize.";
@@ -10,7 +35,7 @@ int main(){
     GLFWmonitor* primaryMonitor = glfwGetPrimaryMonitor();
     const GLFWvidmode* vidMode = glfwGetVideoMode(primaryMonitor);
 
-    GLFWwindow* window = glfwCreateWindow(vidMode->width, vidMode->height,"Window", primaryMonitor, NULL);
+    GLFWwindow* window = glfwCreateWindow(300,300,"Window",NULL, NULL);
 
 
     
@@ -40,4 +65,24 @@ int main(){
 
 
     return 0;
+}
+
+void readSettings(std::map<std::string,struct setting>,std::string fileName){
+
+    std::fstream file;
+    file.open(fileName,std::fstream::out);
+
+    #ifdef _WIN32
+    std::cout << "windows" << std::endl;
+    #endif
+
+    #ifdef linux
+    std::cout << "linux" << std::endl;
+    #endif
+
+
+
+    file << "fuckmeeeeeee";
+
+    file.close();
 }
